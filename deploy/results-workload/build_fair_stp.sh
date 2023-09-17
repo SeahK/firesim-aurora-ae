9c8c9d4d7e61d37dfa74b645d632b2aaa5e90f78
Cloud User <centos@ip-192-168-3-215.ec2.internal>
2023-09-17 23:42:32 +0000

    moving script to script dir as this is gitignored

--- deploy/results-workload/build_fair_stp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python3 parse_fair.py $1/gemmini-tests-workload-xbar/uartlog 0 0 &&
-python3 parse_fair.py $1/gemmini-tests-workload-xbar/uartlog 1 0 &&
-python3 parse_fair.py $1/gemmini-tests-workload-xbar/uartlog 2 0
-python3 parse_fair.py $1/gemmini-tests-workload-noc/uartlog 0 1 &&
-python3 parse_fair.py $1/gemmini-tests-workload-noc/uartlog 1 1 &&
-python3 parse_fair.py $1/gemmini-tests-workload-noc/uartlog 2 1
--- deploy/results-workload/build_xr.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-python3 parse_xr.py $1/gemmini-tests-workload-xbar/uartlog 0
-python3 parse_xr.py $1/gemmini-tests-workload-noc/uartlog 1
